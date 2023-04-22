a37ce10838a1222e9f095e285f9c827c95c3ee57
Jakub Woźniak <jakubwozniak0611@gmail.com>
2023-04-22 20:00:10 +0200

    Fixed code duplicates and renamed some of the mehods and variables in MySparseMatrix_DS2

--- Projekt2/SparseMatrix/src/Main.java
+++ Projekt2/SparseMatrix/src/Main.java
@@ -9,24 +9,8 @@ public class Main
                 {-2,1,2}
         };
         double[] B = {8,-11,-3};
-        MySparseMatrix_DS2 m = new MySparseMatrix_DS2(A);
-        m.solveWithPivot(B);
-        m.solveWithoutPivot(B);
-
-        System.out.println("--------------------------------------------------");
-        double[][] A2 = {
-                {2,-3,-2},
-                {1,-1,2},
-                {-1,2,2}
-        };
-        // double[][] A2 = {
-        //         {2,1,-1},
-        //         {-3,-1,2},
-        //         {-2,1,2}
-        // };
-        double[] B2 = {8,-11,-3};
-        MySparseMatrix_DS3 m2 = new MySparseMatrix_DS3(A2);
-        m2.solveWithPivot(B2);
-        m2.solveWithoutPivot(B2);
+        MySparseMatrix_DS2 m1 = new MySparseMatrix_DS2(A);
+        m1.solveA2(B);
+        m1.solveA1(B);
     }
 }
--- Projekt2/SparseMatrix/src/MySparseMatrix_DS2.java
+++ Projekt2/SparseMatrix/src/MySparseMatrix_DS2.java
@@ -1,14 +1,19 @@
 public class MySparseMatrix_DS2 {
-    private double[][] A;
+    private final double[][] matrix;
 
     public MySparseMatrix_DS2(double[][] A) {
-        this.A = A;
+        this.matrix = A;
     }
-    public void solveWithPivot(double[] B)
-    {
-        int N = B.length;
-        for (int k = 0; k < N; k++)
-        {
+    public void solveA2(double[] BOriginal) {
+        int N = BOriginal.length;
+        double[][] A = new double[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(matrix[i], 0, A[i], 0, N);
+        }
+        double [] B = new double[N];
+        System.arraycopy(BOriginal, 0, B, 0, N);
+
+        for (int k = 0; k < N; k++) {
             int max = k;
             for (int i = k + 1; i < N; i++)
                 if (Math.abs(A[i][k]) > Math.abs(A[max][k]))
@@ -21,27 +26,24 @@ public class MySparseMatrix_DS2 {
                 B[k] = B[max];
                 B[max] = t;
 
+                double div = A[k][k];
+                A[k][k] = 1;
+                for (int j = k + 1; j < N; j++) {
+                    A[k][j] /= div;
+                }
+                B[k] /= div;
+
                 for (int i = k + 1; i < N; i++) {
-                    double factor = A[i][k] / A[k][k];
+                    double factor = A[i][k];
                     A[i][k] = 0;
-                    B[i] -= factor * B[k];
-                    for (int j = k+1; j < N; j++)
+                    for (int j = k + 1; j < N; j++) {
                         A[i][j] -= factor * A[k][j];
+                    }
+                    B[i] -= factor * B[k];
                 }
             }
         }
-        printRowEchelonForm(A, B);
-
-        double[] X = new double[N];
-        for (int i = N - 1; i >= 0; i--)
-        {
-            double sum = 0.0;
-            for (int j = i + 1; j < N; j++)
-                sum += A[i][j] * X[j];
-            X[i] = (B[i] - sum) / A[i][i];
-        }
-
-        printSolution(X);
+        calculateResult(N, A, B);
 
     }
     public void printRowEchelonForm(double[][] A, double[] B)
@@ -65,8 +67,15 @@ public class MySparseMatrix_DS2 {
         System.out.println();
     }
 
-    public void solveWithoutPivot(double[] B) {
-        int N = B.length;
+    public void solveA1(double[] BOriginal) {
+        int N = BOriginal.length;
+
+        double[][] A = new double[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(matrix[i], 0, A[i], 0, N);
+        }
+        double [] B = new double[N];
+        System.arraycopy(BOriginal, 0, B, 0, N);
         for (int k = 0; k < N-1; k++) {
             for (int i = k + 1; i < N; i++) {
                 double factor = A[i][k] / A[k][k];
@@ -76,15 +85,17 @@ public class MySparseMatrix_DS2 {
                 }
             }
         }
-        printRowEchelonForm(A, B);
-    
-        double[] X = new double[N];
-        for (int i = N - 1; i >= 0; i--) {
+        calculateResult(N, A, B);
+    }
+    private void calculateResult(int n, double[][] a, double[] b) {
+        printRowEchelonForm(a, b);
+        double[] X = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
-            for (int j = i + 1; j < N; j++) {
-                sum += A[i][j] * X[j];
+            for (int j = i + 1; j < n; j++) {
+                sum += a[i][j] * X[j];
             }
-            X[i] = (B[i] - sum) / A[i][i];
+            X[i] = (b[i] - sum) / a[i][i];
         }
         printSolution(X);
     }
