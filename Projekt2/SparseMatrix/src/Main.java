ffa781ec2501c2b956e0fec0308e6c3b1cc4ba7c
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-04-23 15:55:20 +0200

    Added random number generator

--- Projekt2/SparseMatrix/src/Main.java
+++ Projekt2/SparseMatrix/src/Main.java
@@ -12,5 +12,14 @@ public class Main
         MySparseMatrix_DS2 m1 = new MySparseMatrix_DS2(A);
         m1.solveA2(B);
         m1.solveA1(B);
+
+        double[][] A2 = {
+                {2,-3,-2},
+                {1,-1,1},
+                {-1,2,2}
+        };
+        double[] B2 = {8,-11,-3};
+        MySparseMatrix_DS3 m2 = new MySparseMatrix_DS3(A2);
+        m2.solveA1(B2);
     }
 }
--- Projekt2/SparseMatrix/src/MySparseMatrix_DS3.java
+++ Projekt2/SparseMatrix/src/MySparseMatrix_DS3.java
@@ -1,58 +1,62 @@
-import java.util.*;
-
 public class MySparseMatrix_DS3 {
-    private List<List<Double>> columns;
+    private final double[][] matrix;
 
-    public MySparseMatrix_DS3(List<List<Double>> columns) {
-        this.columns = columns;
+    public MySparseMatrix_DS3(double[][] A) {
+        this.matrix = A;
     }
+    public void solveA2(double[] BOriginal) {
+        int N = BOriginal.length;
+        double[][] A = new double[N][N];
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < N; i++) {
+                A[i][j] = matrix[i][j];
+            }
+        }
+        double [] B = new double[N];
+        System.arraycopy(BOriginal, 0, B, 0, N);
 
-    public void solveWithPivot(double[] B) {
-        int N = B.length;
         for (int k = 0; k < N; k++) {
             int max = k;
-            for (int i = k + 1; i < N; i++) {
-                if (Math.abs(columns.get(i).get(k)) > Math.abs(columns.get(max).get(k))) {
+            for (int i = k + 1; i < N; i++)
+                if (Math.abs(A[i][k]) > Math.abs(A[max][k]))
                     max = i;
-                }
-            }
-            if (columns.get(max).get(k) != 0) {
-                Collections.swap(columns, k, max);
+            if(A[max][k] != 0) {
+                double[] temp = A[k];
+                A[k] = A[max];
+                A[max] = temp;
                 double t = B[k];
                 B[k] = B[max];
                 B[max] = t;
 
+                double div = A[k][k];
+                A[k][k] = 1;
                 for (int i = k + 1; i < N; i++) {
-                    double factor = columns.get(i).get(k) / columns.get(k).get(k);
-                    columns.get(i).set(k, 0.0);
-                    B[i] -= factor * B[k];
-                    for (int j = k+1; j < N; j++) {
-                        columns.get(i).set(j, columns.get(i).get(j) - factor * columns.get(k).get(j));
+                    A[i][k] /= div;
+                }
+                B[k] /= div;
+
+                for (int j = k + 1; j < N; j++) {
+                    double factor = A[k][j];
+                    A[k][j] = 0;
+                    for (int i = k + 1; i < N; i++) {
+                        A[i][j] -= factor * A[i][k];
                     }
+                    B[j] -= factor * B[k];
                 }
             }
-        }
-        printRowEchelonForm(columns, B);
 
-        double[] X = new double[N];
-        for (int i = N - 1; i >= 0; i--) {
-            double sum = 0.0;
-            for (int j = i + 1; j < N; j++) {
-                sum += columns.get(j).get(i) * X[j];
-            }
-            X[i] = (B[i] - sum) / columns.get(i).get(i);
         }
+        calculateResult(N, A, B);
 
-        printSolution(X);
     }
-
-    public void printRowEchelonForm(List<List<Double>> columns, double[] B) {
+    public void printRowEchelonForm(double[][] A, double[] B)
+    {
         int N = B.length;
         System.out.println("\nRow Echelon form : ");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.printf("%.3f ", columns.get(j).get(i));
-            }
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                System.out.printf("%.3f ", A[i][j]);
             System.out.printf("| %.3f\n", B[i]);
         }
         System.out.println();
@@ -61,34 +65,41 @@ public class MySparseMatrix_DS3 {
     public void printSolution(double[] X) {
         int N = X.length;
         System.out.println("Solution : ");
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
             System.out.printf("x%d = %.3f ", i, X[i]);
-        }
         System.out.println();
     }
 
-    public void solveWithoutPivot(double[] B) {
-        int N = B.length;
+    public void solveA1(double[] BOriginal) {
+        int N = BOriginal.length;
+
+        double[][] A = new double[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(matrix[i], 0, A[i], 0, N);
+        }
+        double [] B = new double[N];
+        System.arraycopy(BOriginal, 0, B, 0, N);
         for (int k = 0; k < N-1; k++) {
             for (int i = k + 1; i < N; i++) {
-                double factor = columns.get(i).get(k) / columns.get(k).get(k);
+                double factor = A[i][k] / A[k][k];
                 B[i] -= factor * B[k];
                 for (int j = k; j < N; j++) {
-                    columns.get(i).set(j, columns.get(i).get(j ) - factor * columns.get(k).get(j));
+                    A[i][j] -= factor * A[k][j];
                 }
             }
         }
-        printRowEchelonForm(columns, B);
-
-        double[] X = new double[N];
-        for (int i = N - 1; i >= 0; i--) {
+        calculateResult(N, A, B);
+    }
+    private void calculateResult(int n, double[][] a, double[] b) {
+        printRowEchelonForm(a, b);
+        double[] X = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
-            for (int j = i + 1; j < N; j++) {
-                sum += columns.get(j).get(i) * X[j];
+            for (int j = i + 1; j < n; j++) {
+                sum += a[i][j] * X[j];
             }
-            X[i] = (B[i] - sum) / columns.get(i).get(i);
+            X[i] = (b[i] - sum) / a[i][i];
         }
-
         printSolution(X);
     }
 }
